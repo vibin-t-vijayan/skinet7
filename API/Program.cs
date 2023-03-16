4210754d431314be02d41b412cada8d482d18ff4
vibin-t-vijayane <m.vibintvijayan@gmail.com>
2023-03-16 10:45:49 +0530

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
-
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(Genericrepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
